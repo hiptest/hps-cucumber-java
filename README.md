911fc883e8e42bbfe4a9728f6f4014c3fcd5384c
Vincent Pretre <vincent.pretre@gmail.com>
2016-02-23 15:30:45 +0100

    Updated README and configured Travis

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # hps-cucumber-java
-Hiptest publisher samples for Cucumber/Java
+
+[![Build Status](https://travis-ci.org/hiptest/hps-cucumber-java.svg?branch=master)](https://travis-ci.org/hiptest/hps-cucumber-java)
+
+Hiptest publisher samples with Cucumber/Java
+
+In this repository you'll find tests generated in Cucumber/Java format from [Hiptest](https://hiptest.net), using [Hiptest publisher](https://github.com/hiptest/hiptest-publisher).
+
+The goals are:
+
+ * to show how tests are exported in Cucumber/Java.
+ * to check exports work out of the box (well, with implemented actionwords)
+
+System under test
+------------------
+
+The SUT is a (not that much) simple coffee machine. You start it, you ask for a coffee and you get it, sometimes. But most of times you have to add water or beans, empty the grounds. You have an automatic expresso machine at work or at home? So you know how it goes :-)
+
+Update tests
+-------------
+
+
+To update the tests:
+
+    "hiptest-publisher -c cucumber-java.conf --only=features,step_definitions"
+
+The tests are generated in [``src/main/test/java/com/coffeemachine``](https://github.com/hiptest/hps-cucumber-java/tree/master/src/test/java/com/coffeemachine)
+
+Run tests
+---------
+
+
+To build the project and run the tests, use the following command:
+
+    mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+    mvn test -B
+
+The SUT implementation can be seen in [``src/main/java/com/coffeemachine/CoffeeMachine.java``](https://github.com/hiptest/hps-cucumber-java/blob/master/src/main/java/com/coffeemachine/CoffeeMachine.java)
+
+The test report is generated in ```target/surefire-reports/TEST-com.coffeemachine.CoffeeMachineTest.xml```
--- src/test/java/com/coffeemachine/StepDefinitions.java
+++ src/test/java/com/coffeemachine/StepDefinitions.java
@@ -1,36 +1,36 @@
 package com.coffeemachine;
 
-import cucumber.api.java.en.Given;
+import cucumber.api.java.en.*;
 
 public class StepDefinitions {
     public Actionwords actionwords = new Actionwords();
 
-    @Given("^I start the coffee machine \"(.*)\"$")
+    @When("^I start the coffee machine \"(.*)\"$")
     public void iStartTheCoffeeMachine(String lang) {
         actionwords.iStartTheCoffeeMachine(lang);
     }
 
-    @Given("^I shutdown the coffee machine$")
+    @When("^I shutdown the coffee machine$")
     public void iShutdownTheCoffeeMachine() {
         actionwords.iShutdownTheCoffeeMachine();
     }
 
-    @Given("^message \"(.*)\" should be displayed$")
+    @Then("^message \"(.*)\" should be displayed$")
     public void messageMessageShouldBeDisplayed(String message) {
         actionwords.messageMessageShouldBeDisplayed(message);
     }
 
-    @Given("^coffee should be served$")
+    @Then("^coffee should be served$")
     public void coffeeShouldBeServed() {
         actionwords.coffeeShouldBeServed();
     }
 
-    @Given("^coffee should not be served$")
+    @Then("^coffee should not be served$")
     public void coffeeShouldNotBeServed() {
         actionwords.coffeeShouldNotBeServed();
     }
 
-    @Given("^I take a coffee$")
+    @When("^I take a coffee$")
     public void iTakeACoffee() {
         actionwords.iTakeACoffee();
     }
@@ -40,17 +40,17 @@ public class StepDefinitions {
         actionwords.iEmptyTheCoffeeGrounds();
     }
 
-    @Given("^I fill the beans tank$")
+    @When("^I fill the beans tank$")
     public void iFillTheBeansTank() {
         actionwords.iFillTheBeansTank();
     }
 
-    @Given("^I fill the water tank$")
+    @When("^I fill the water tank$")
     public void iFillTheWaterTank() {
         actionwords.iFillTheWaterTank();
     }
 
-    @Given("^I take \"(.*)\" coffees$")
+    @When("^I take \"(.*)\" coffees$")
     public void iTakeCoffeeNumberCoffees(int coffeeNumber) {
         actionwords.iTakeCoffeeNumberCoffees(coffeeNumber);
     }
@@ -60,12 +60,12 @@ public class StepDefinitions {
         actionwords.theCoffeeMachineIsStarted();
     }
 
-    @Given("^fifty coffees have been taken without filling the tank$")
+    @When("^fifty coffees have been taken without filling the tank$")
     public void fiftyCoffeesHaveBeenTakenWithoutFillingTheTank() {
         actionwords.fiftyCoffeesHaveBeenTakenWithoutFillingTheTank();
     }
 
-    @Given("^thirty eight coffees are taken without filling beans$")
+    @When("^thirty eight coffees are taken without filling beans$")
     public void thirtyEightCoffeesAreTakenWithoutFillingBeans() {
         actionwords.thirtyEightCoffeesAreTakenWithoutFillingBeans();
     }
